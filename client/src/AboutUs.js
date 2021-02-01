b3e124f05f6cee6332ec99f83451fc06a4a8975d
Unnati <unnat4ucreative@gmail.com>
2021-02-01 14:04:37 +0530

    Setting up home page and other pages1

---
+++ client/src/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutUs(){
+  return(
+    <div>
+      <h1>About Us here!!!</h1>
+    </div>
+  )
+}
+
+export default AboutUs;
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home.js';
+import Main from './Main.js';
+import ContactUs from './ContactUs.js';
+import Blog from './Blog.js';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route,Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/main">Main</Link>
+        <Link to="/contactus">Contact Us</Link>
+        <Link to="/blog">Blog</Link>
+
+          <Switch>
+            <Route exact path="/" ><Home /></Route>
+            <Route path="/main" ><Main /></Route>
+            <Route path="/contactus" ><ContactUs /></Route>
+            <Route path="/blog" ><Blog /></Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
