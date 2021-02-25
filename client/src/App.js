2880c31541d2fa79052990d86dc9afdc54bd942c
devanshi-katyal <60283765+devanshi-katyal@users.noreply.github.com>
2021-02-25 14:51:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
-import {
-  Route,
-  Link
-} from 'react-router-dom';
-
-  import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-
 import Home from './Home.js';
 import Main from './Main.js';
 import ContactUs from './ContactUs.js';
@@ -13,43 +7,24 @@ import Blog from './Blog.js';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route,Link} from "react-router-dom";
 
-
-import Home from './components/Home';
-import About from './components/About';
-import Messages from './components/Messages';
 function App() {
- 
   return (
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/main">Main</Link>
+        <Link to="/contactus">Contact Us</Link>
+        <Link to="/blog">Blog</Link>
 
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div className="menu">
-      <ul>
-<li> <Link to="/">Home</Link> </li>
-<li> <Link to="/messages">Messages</Link> </li>
-<li> <Link to="/about">About</Link> </li>
-{/* <li>
-    <a href="/messages">Messages (with a regular anchor element)</a> 
-  </li> */}
-</ul>
-    </div>
-    <div className="App-intro">
-          <Route path="/" component={Home}/>
-          <Route path="/messages" component={Messages}/>
-          <Route path="/about" component={About}/>
-        </div>
-    </div>
-
+          <Switch>
+            <Route exact path="/" ><Home /></Route>
+            <Route path="/main" ><Main /></Route>
+            <Route path="/contactus" ><ContactUs /></Route>
+            <Route path="/blog" ><Blog /></Route>
+          </Switch>
+      </div>
+    </Router>
   );
- }
+}
+
 export default App;
